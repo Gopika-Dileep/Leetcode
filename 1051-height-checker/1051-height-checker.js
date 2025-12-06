133b047dd8a3ef2849e45d03175f86f93dcf3678
Gopika-Dileep <gopikadileep76@gmail.com>
2025-12-06 18:46:39 +0530

    Time: 1 ms (54.3%), Space: 54.4 MB (60.38%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -2,11 +2,11 @@
  * @param {number[]} heights
  * @return {number}
  */
-var heightChecker = function(heights) {
-    let count=0
-    let sorts =[...heights].sort((a,b)=>a-b)
-    for(let i =0 ; i<heights.length;i++){
-        if(heights[i]!==sorts[i]){
+var heightChecker = function (heights) {
+    let count = 0
+    let sorts = [...heights].sort((a, b) => a - b)
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== sorts[i]) {
             count++
         }
     }
