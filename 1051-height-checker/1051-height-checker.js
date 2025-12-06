2179fc5bb05c45eaefca6573f0ae90d92ed877ee
Gopika-Dileep <gopikadileep76@gmail.com>
2025-12-06 18:46:19 +0530

    Time: 1 ms (54.3%), Space: 54.4 MB (52.41%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let count=0
+    let sorts =[...heights].sort((a,b)=>a-b)
+    for(let i =0 ; i<heights.length;i++){
+        if(heights[i]!==sorts[i]){
+            count++
+        }
+    }
+    return count
+};
