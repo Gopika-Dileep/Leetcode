b3243bc02a5f34450d5152fd08fafae06849367e
Gopika-Dileep <gopikadileep76@gmail.com>
2025-12-18 20:43:52 +0530

    Time: 0 ms (100%), Space: 53.8 MB (80.64%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function (gain) {
+    let highest = 0
+    let sum = 0
+    for (let i = 0; i < gain.length; i++) {
+        sum += gain[i]
+        highest = Math.max(highest, sum)
+    }
+    return highest
+};
