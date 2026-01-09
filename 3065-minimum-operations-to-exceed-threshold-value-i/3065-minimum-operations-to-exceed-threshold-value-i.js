aaa0675eb30d6c7785236b9fb17e9d920e8511f0
Gopika-Dileep <gopikadileep76@gmail.com>
2026-01-09 12:56:36 +0530

    Time: 0 ms (100%), Space: 55.3 MB (83.96%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
@@ -4,5 +4,12 @@
  * @return {number}
  */
 var minOperations = function (nums, k) {
-    return nums.filter((num) => num < k).length
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < k) {
+            count++;
+        }
+    }
+
+    return count;
 };
