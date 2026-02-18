4ae707a7177862a4bf84c4f65264e345062afa8a
Gopika-Dileep <gopikadileep76@gmail.com>
2026-02-18 23:27:10 +0530

    Time: 1 ms (63.27%), Space: 55.5 MB (73.47%) - LeetHub

---
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumChairs = function (s) {
+    let res = 0, max = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'E') {
+            res++;
+        } else {
+            max = Math.max(res, max);
+            res--;
+        }
+    }
+    max = Math.max(res, max);
+    return max;
+
+};
