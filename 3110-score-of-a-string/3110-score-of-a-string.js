23d33111687b4557a2febf42e60be094a4bbd478
Gopika-Dileep <gopikadileep76@gmail.com>
2026-02-19 20:34:54 +0530

    Time: 0 ms (100%), Space: 57.4 MB (14.29%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let res = 0
-    for(let i = 0 ; i<s.length-1;i++){
-        res+=Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1))
-    }
-    return res
+let sum =0
+for(let i =0 ; i<s.length-1;i++){
+    sum+=Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1))
+}
+return sum
 };
