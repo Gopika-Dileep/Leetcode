e31b62851d8494c2706d553492037aa61103eae0
Gopika-Dileep <gopikadileep76@gmail.com>
2026-03-05 23:23:21 +0530

    Time: 5 ms (35.95%), Space: 56.3 MB (79.74%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
@@ -3,13 +3,15 @@
  * @param {number[]} students
  * @return {number}
  */
-var minMovesToSeat = function (seats, students) {
-    let arr1 = seats.sort((a, b) => a - b)
-    let arr2 = students.sort((a, b) => a - b)
-    let sum = 0
-    console.log(arr1, arr2)
-    for (let i = 0; i < arr1.length; i++) {
-        sum += Math.abs(arr1[i] - arr2[i])
+var minMovesToSeat = function(seats, students) {
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b);
+
+    let moves = 0;
+
+    for (let i = 0; i < seats.length; i++) {
+        moves += Math.abs(seats[i] - students[i]);
     }
-    return sum
+
+    return moves;
 };
