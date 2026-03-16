eead21447c6692a2830d83f2f9aeece6047c188f
Gopika Dileep <gopikadileep76@gmail.com>
2026-03-16 21:52:05 +0530

    Time: 48 ms (21.46%), Space: 67.8 MB (8.11%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+    let map = { 0: 1 };
+    let sum = 0;
+    let count = 0;
+
+    for (let num of nums) {
+        sum += num;
+
+        if (map[sum - k]) {
+            count += map[sum - k];
+        }
+
+        map[sum] = (map[sum] || 0) + 1;
+    }
+
+    return count;
+
+};
