42e1b93173326f42423d44a1d9f0267b2e93755a
Gopika Dileep <gopikadileep76@gmail.com>
2026-03-20 12:55:56 +0530

    Time: 8 ms (30.24%), Space: 58.3 MB (21.43%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    let freq = {}
+    for (let num of nums) {
+        freq[num] = (freq[num] || 0) + 1
+    }
+
+    for (let key in freq) {
+        if (freq[key] == 1) {
+            return Number(key)
+        }
+    }
+
+};
