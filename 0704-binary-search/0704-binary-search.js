822e06d45eca5c5270f26589e333547d33ca1df5
Gopika Dileep <gopikadileep76@gmail.com>
2026-04-23 19:05:29 +0530

    Time: 0 ms (100%), Space: 58.2 MB (49.18%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0
+    let right = nums.length
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2)
+        if (target == nums[middle]) {
+            return middle
+        }
+        if (target < nums[middle]) {
+            right = middle - 1
+        } else {
+            left = middle + 1
+        }
+    }
+    return -1
+
+};
